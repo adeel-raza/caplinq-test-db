b6aaa9ad4fd5cb780360c23e7e72a6662cb9f928
webxity <sid@webxity.com>
2014-10-27 23:07:30 +0500

    asas

--- test.sql
+++ test.sql
@@ -23,8 +23,8 @@ DROP TABLE IF EXISTS `test`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `test` (
-  `aaaa` int(11) NOT NULL,
-  `ffdd` int(11) NOT NULL
+  `aaaa` bigint(11) NOT NULL,
+  `dddd` bigint(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -34,7 +34,6 @@ CREATE TABLE `test` (
 
 LOCK TABLES `test` WRITE;
 /*!40000 ALTER TABLE `test` DISABLE KEYS */;
-INSERT INTO `test` VALUES (5555,55555555555),(5555,555555555555);
 /*!40000 ALTER TABLE `test` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -47,4 +46,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2014-10-27 22:58:07
+-- Dump completed on 2014-10-27 22:53:30
